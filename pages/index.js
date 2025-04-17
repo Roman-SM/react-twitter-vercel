fd2b9b0d2b334ca5cf074ceee6cd7888110b8b4f
Рома <roma@MacBook-Air-Roma.local>
2025-04-17 19:38:38 +0300

    UPD Code

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,5 @@
 import ReactDOM from "react-dom/client";
-
-import "./normalize.css";
-import "./index.css";
-
-import App from "./App";
+import App from "./_app";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
