0db646ae90907f213ddf3553f69f69d8bbfda185
Рома <roma@MacBook-Air-Roma.local>
2025-04-17 19:50:11 +0300

    UPD Code

--- pages/_app.js
+++ pages/_app.js
@@ -1,18 +1,10 @@
 import dynamic from "next/dynamic";
 
-// Динамічно імпортуємо компонент з відключенням SSR
 const PostList = dynamic(() => import("../container/post-list"), {
-  ssr: false, // Відключаємо SSR для цього компонента
+  ssr: false, // Відключаємо серверний рендеринг для цього компонента
 });
 
 function App() {
-  useEffect(() => {
-    // Тільки на клієнті доступно document
-    if (typeof document !== "undefined") {
-      console.log("App is rendered on the client-side");
-    }
-  }, []);
-
   return (
     <Page>
       <PostList />
