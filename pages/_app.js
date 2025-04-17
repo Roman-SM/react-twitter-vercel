cfc5266ff9973ca47dec947a36da333de421b4e6
Рома <roma@MacBook-Air-Roma.local>
2025-04-17 19:44:28 +0300

    UPD Code

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,17 @@
+import React, { useEffect } from "react";
 import Page from "../component/page";
 import PostList from "../container/post-list";
 import "./index.css";
 import "./normalize.css";
 
 function App() {
+  useEffect(() => {
+    // Тільки на клієнті доступно document
+    if (typeof document !== "undefined") {
+      console.log("App is rendered on the client-side");
+    }
+  }, []);
+
   return (
     <Page>
       <PostList />
